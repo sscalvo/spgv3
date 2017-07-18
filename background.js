b9c1b2bc4db037acf47033688848b6b33b4db548
sscalvo <zergios@yahoo.com>
2017-07-18 20:15:06 +0200

    More files from v2 added
    Few more files added from v2

--- background.js
+++ background.js
@@ -1,10 +1,13 @@
 //https://developer.chrome.com/extensions/event_pages  BEST PRACTICES FOR background.js
 //GLOBAL
-var url_CALM_course=""; //content.js will use it for ajax-quering calm server 
+var url_DDBS_course=""; //content.js will use it for ajax-quering calm server 
+var regex_CALM_production = /https:\/\/calm\.dhamma\.org\/(\w|-)+\/courses\/\d+\/course_applications*/; //All CALM4 languages
+var regex_CALM_test = /https:\/\/test\.calm\.dhamma\.org\/(\w|-)+\/courses\/\d+\/course_applications*/; //SPGv2
+var regex_VRAY_production = ""; //add others
 
 ////////////////////////////////////Enable extension only on valid URL's (the CALM4 courses)
 //chrome.browserAction.setIcon({path: {"19": "icons/enable_19.png", "38": "icons/enable_38.png"}});
-/* function disableBrowserAction(){
+function disableBrowserAction(){
     chrome.browserAction.setIcon({path:"disabled.png"});
     chrome.browserAction.setTitle({title:"Select a CALM4 webpage to ACTIVATE"});
 	chrome.browserAction.setPopup({popup: "popup_disabled.html"});
@@ -16,21 +19,19 @@ function enableBrowserAction(){
     chrome.browserAction.setTitle({title:"Seating Plan Generator"});
 	chrome.browserAction.setPopup({popup: "popup_enabled.html"});
     //chrome.tabs.executeScript(null, {file: "contentscript.js"});
-} */
+}
 
 function updateState(url){
-	var regExCALM_produccion = /https:\/\/calm\.dhamma\.org\/(\w|-)+\/courses\/\d+\/course_applications*/; //All CALM4 languages
-	var regExCALM_test = /https:\/\/test\.calm\.dhamma\.org\/(\w|-)+\/courses\/\d+\/course_applications*/; //SPGv2
 	//var regExCALM_desarrollo = /http:\/\/localhost*/;
-	var enabled =  regExCALM_test.test(url) || regExCALM_produccion.test(url); //regExCALM_desarrollo.test(url) ||
+	var enabled =  regex_CALM_test.test(url) || regex_CALM_production.test(url); //regExCALM_desarrollo.test(url) ||
 	if(enabled){ //URL of a CALM course!
 		//Consider this *valid* url: https://test.calm.dhamma.org/en/courses/119/course_applications/8/edit#day-0 
 		var app = "applications"; //lets trim the "/8/edit#day-0" (added by CAL when user navigates through CALM inner tabs) 
-		url_CALM_course = url.substr(0,url.search(app) + app.length);;  //update the url
-		console.log("background.js: VALID URL: " + url_CALM_course);
-//		enableBrowserAction(); //Active icon
+		url_DDBS_course = url.substr(0,url.search(app) + app.length);;  //update the url
+		console.log("background.js: VALID URL: " + url_DDBS_course);
+		enableBrowserAction(); //Active icon
 	}else{
-//		disableBrowserAction();  //Disable icon
+		disableBrowserAction();  //Disable icon
 	}
 }
 
@@ -53,10 +54,8 @@ chrome.tabs.onActivated.addListener(function(evt){
 **/
   chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-	  //console.log("Message received: " + request.command);
     if (request.command == "newTab"){  //mensage from content.js
 		chrome.tabs.create({ url: request.url}, function(tab){
-					//console.log("Created a tab");
 					//sendResponse({farewell: "Created a tab"});
 
 		});
---
+++ content.js
@@ -0,0 +1,50 @@
+var id_button_injection = {"CALM":"course-applications-datatable_filter", "VRAY":"some_html_id"};
+
+/**GLIPHICON BUTTON to OPEN NEW TAB
+* Inject a button into the CALM webpages. Final html output should look like this:
+    <div id="div_spg_button">
+		<label for="mySubmit" class="btn" onclick="...."><i class="glyphicon glyphicon-equalizer" ></i> Hall </label>
+		<input id="spg_button" type="button" value="Go" class="hidden" />
+	</div>
+	On the 'onclick' event of this button is where happens the begining of SPG
+*/
+
+	var span_button = document.createElement('span'); //<div ..>
+	span_button.id = "div_spg_button";
+	
+	var label_btn = document.createElement('label');  //<label..>
+	label_btn.setAttribute("for", "spg_button");
+    label_btn.className ="btn";
+	label_btn.innerHTML ="<span style='background-color: yellow;'>&nbsp;<i class='glyphicon glyphicon-equalizer'></i> Hall map&nbsp;</span>"; //No need for label.createElement("i")
+	
+	label_btn.onclick = function(){ //Here BEGINS the creation of the SPG tab
+		try{
+			var path = chrome.extension.getURL("css/sitPlan.css"); //When CALM session has expired, this line throwed a nasty error
+		}
+		catch(err){
+			console.log(err);
+			window.location.reload();
+		}
+		//Ask background.js to open a new tab for us..
+		chrome.runtime.sendMessage({url : chrome.extension.getURL("html/sp_tab.html"), command: "newTab"}, function(response) {
+			console.log("Content.js requested NEW TAB. background.js says: " +response.farewell);
+		});
+	}
+	//var i_label = document.createElement("i");  //<i.. >
+	//i_label.className = "glyphicon glyphicon-equalizer";
+	
+	var button_SPG = document.createElement('input'); //<input..>
+	button_SPG.type  = 'button';
+	button_SPG.value = "Generate Seating Plan";
+	button_SPG.style = "background-color: LimeGreen";
+	button_SPG.style.marginRight =  "15px";
+	button_SPG.id = "spg_button";
+	button_SPG.className += "hidden";
+
+	span_button.appendChild(label_btn);
+	span_button.appendChild(button_SPG);
+//END    GLIPHICON BUTTON to OPEN NEW TAB
+
+//Add button to CALM-course-page and wait for the user to click it!
+document.getElementById(id_button_injection.CALM).appendChild(span_button);
+//TODO: Confirm with Ryan about the existence of "course-applications-datatable_filter" -> Find a new place for the button?
